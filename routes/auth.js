d38c56e1e8e2144af52daa931e22f806416d8539
Gautam Vishwakarma <vishkarmaamrawati9@gmail.com>
2026-07-10 10:10:09 +0530

    fixed some problems

--- routes/auth.js
+++ routes/auth.js
@@ -8,8 +8,8 @@ router.get("/login",passport.authenticate("google", { scope: ["email", "profile"
 router.get(
   "/google/callback",
   passport.authenticate("google", {
-    successRedirect: process.env.FRONTEND_URL||"http://localhost:5173",
-    failureRedirect: (process.env.FRONTEND_URL+'/login')||"http://localhost:5173/login",
+    successRedirect: `|${process.env.FRONTEND_URL||"http://localhost:5173"}`,
+    failureRedirect: `${process.env.FRONTEND_URL||"http://localhost:5173"}/login`
   })
 );
 
