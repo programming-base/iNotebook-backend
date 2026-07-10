eb5ceb4531099dec835846a305bf4fc766a50924
Gautam Vishwakarma <vishkarmaamrawati9@gmail.com>
2026-07-10 10:42:23 +0530

    Fix Render OAuth session config

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ import notes from "./routes/notes.js";
 import { isAuthenticated } from "./middlewares/isAuthenticated.js";
 const app = express();
 
+app.set("trust proxy", 1);
+
 app.use(cors({
   origin: process.env.FRONTEND_URL||"http://localhost:5173",
   credentials: true
@@ -23,6 +25,11 @@ app.use(session({
   secret:process.env.SESSION_SECRET,
   resave:false,
   saveUninitialized:false,
+  proxy:true,
+  cookie:{
+    sameSite:"none",
+    secure:true,
+  },
   store: MongoStore.create(
     {
       mongoUrl:process.env.MONGO_URI||"mongodb://localhost:27017/inotebook",
@@ -38,6 +45,8 @@ app.use("/notes",notes);
 app.get("/",isAuthenticated,(req,res)=>{
   res.json({user:req.user,isAuthenticated:true})
 })
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
