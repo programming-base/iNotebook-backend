5714fc894a5037b62955f49f859ddd076917b205
Gautam Vishwakarma <vishkarmaamrawati9@gmail.com>
2026-07-10 10:19:38 +0530

    updated the callback url

--- config/passport.js
+++ config/passport.js
@@ -3,12 +3,13 @@ import passport from "passport";
 import {Strategy as GoogleStrategy} from "passport-google-oauth20";
 import dotenv from "dotenv";
 dotenv.config();
+const backendUrl = process.env.BACKEND_URL || "http://localhost:3000";
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback"
+      callbackURL: `${backendUrl}/auth/google/callback`
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
